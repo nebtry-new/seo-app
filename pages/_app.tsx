77895fc6781cd11bdfdbb3d5fa028796c3ca96d8
Pik <pikzaa@gmail.com>
2023-02-22 20:52:27 +0700

    use: getServerSideProps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,46 +1,5 @@
-import "@/styles/globals.css";
-import axios from "axios";
 import type { AppProps } from "next/app";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const fetchData = async () => {
-    const resp = await axios.get("https://www.boredapi.com/api/activity");
-    if (resp.data) {
-      if (router.asPath.includes("first")) {
-        resp.data = {
-          activity: "Mission Transit c700",
-          key: "Redirect Link 1",
-          url: "https://file-paotang-uat.th-service.co.in/DEEPLINK/paotang-external-deeplink.html?destination=missionWeb&page=tracking&campaignCode=c700",
-          image:
-            "https://mission-static-file-uat.storage.googleapis.com/mock/campaign-transit.png",
-        };
-      } else if (router.asPath.includes("second")) {
-        resp.data = {
-          activity: "Mission Landing",
-          key: "Redirect Link 2",
-          url: "https://file-paotang-uat.th-service.co.in/DEEPLINK/paotang-external-deeplink.html?destination=missionWeb",
-          image:
-            "https://mission-static-file-uat.storage.googleapis.com/mock/bg-landing.png",
-        };
-      } else if (router.asPath.includes("third")) {
-        resp.data = {
-          activity: "Mission Wallet-Topup c795",
-          key: "Redirect Link 3",
-          url: "https://file-paotang-uat.th-service.co.in/DEEPLINK/paotang-external-deeplink.html?destination=missionWeb&page=tracking&campaignCode=c795",
-          image:
-            "https://mission-static-file-uat.storage.googleapis.com/mock/transaction-count-paybill.png",
-        };
-      }
-      return resp.data;
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  });
-
   return <Component {...pageProps} />;
 }
--- pages/second.tsx
+++ pages/second.tsx
@@ -1,60 +1,43 @@
-import { commonInfoRender } from "@/common/util";
 import styles from "@/styles/Home.module.css";
+import axios from "axios";
 import Head from "next/head";
 
-export default function Second() {
-  const MetaTag = () => {
-    // if (typeof window !== "undefined") {
-    //   const data = sessionStorage.getItem("meta");
-
-    //   if (data) {
-    //     const apiResponse = JSON.parse(data);
-
-    //     if (apiResponse !== undefined) {
-    //       return (
-    //         <Head>
-    //           <title>{commonInfoRender(apiResponse.activity)}</title>
-    //           <meta
-    //             name="description"
-    //             content={commonInfoRender(apiResponse.key)}
-    //           />
-    //           <meta
-    //             property="og:title"
-    //             content={commonInfoRender(apiResponse.activity)}
-    //           />
-    //           <meta
-    //             property="og:description"
-    //             content={commonInfoRender(apiResponse.key)}
-    //           />
-    //           <meta
-    //             property="og:image"
-    //             content={commonInfoRender(apiResponse.image)}
-    //           />
-    //           <meta
-    //             property="og:url"
-    //             content={commonInfoRender(apiResponse.url)}
-    //           />
-    //           <meta
-    //             http-equiv="refresh"
-    //             content={`1; url=${commonInfoRender(apiResponse.url)}`}
-    //           ></meta>
-    //           <meta property="og:type" content="website" />
-    //           <link rel="icon" href="/favicon.ico" />
-    //         </Head>
-    //       );
-    //     }
-    //   }
-    // }
-    return (
+export default function Second({ data }) {
+  return (
+    <>
+      <Head>
+        <title>{data.activity}</title>
+        <meta name="description" content={data.key} />
+        <meta property="og:title" content={data.activity} />
+        <meta property="og:description" content={data.key} />
+        <meta property="og:image" content={data.image} />
+        <meta property="og:url" content={data.url} />
+        <meta http-equiv="refresh" content={`1; url=${data.url}`}></meta>
+        <meta property="og:type" content="website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <main className={styles.main}>
         <div className={styles.title}>Redirecting...</div>
       </main>
-    );
-  };
-
-  return (
-    <>
-      <MetaTag></MetaTag>
     </>
   );
+}
+
+export async function getServerSideProps() {
+  let { data } = await axios.get("https://www.boredapi.com/api/activity");
+  if (data) {
+    data = {
+      activity: data.activity,
+      key: data.key,
+      url: "https://file-paotang-uat.th-service.co.in/DEEPLINK/paotang-external-deeplink.html?destination=missionWeb",
+      image:
+        "https://mission-static-file-uat.storage.googleapis.com/mock/bg-landing.png",
+    };
+
+    return {
+      props: {
+        data,
+      },
+    };
+  }
 }
